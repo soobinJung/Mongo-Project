4f986545fdd7698a0116aedf19979d120ccc37eb
soobinJung <66097044+soobinJung@users.noreply.github.com>
2022-03-27 13:35:04 +0900

    controller 변경

--- src/main/java/com/noti/api/notice/controller/NoticeController.java
+++ src/main/java/com/noti/api/notice/controller/NoticeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,73 +26,76 @@ import lombok.AllArgsConstructor;
 @RequestMapping("notice")
 @RestController
 public class NoticeController {
+   
+   NoticeService service;
+   
+   @GetMapping("")
+   @ResponseBody
+   public ResponseEntity<Object> getNoticeByAll(NoticeSearch noticeSearch) {
+      return ResponseEntity.ok().body(
+         service.getNoticeByAll(noticeSearch)
+      );
+   }
 
-	NoticeService service;
-	
-	@GetMapping("")
-	@ResponseBody
-	public ResponseEntity<Object> getNoticeByAll(NoticeSearch noticeSearch) {
-		return ResponseEntity.ok().body(
-			service.getNoticeByAll(noticeSearch)
-		);
-	}
+   @GetMapping("/{noticeId}")
+   @ResponseBody
+   public ResponseEntity<NoticeDto> getNoticeByNoticeId(@PathVariable(name = "noticeId", required = true) long noticeId) {
+      
+      return ResponseEntity.ok().body(
+         service.getNoticeById(noticeId)
+      );
+   }
+   
+   @PostMapping("")
+   @ResponseBody
+   public ResponseEntity<NoticeDto> insertNotice(@RequestBody NoticeDto noticeDto) throws AuthenticationException {
 
-	@GetMapping("/{noticeId}")
-	@ResponseBody
-	public ResponseEntity<NoticeDto> getNoticeByNoticeId(@PathVariable(name = "noticeId", required = true) long noticeId) {
-		return ResponseEntity.ok().body(
-			service.getNoticeById(noticeId)
-		);
-	}
-	
-	@PostMapping("")
-	@ResponseBody
-	public ResponseEntity<NoticeDto> insertNotice(NoticeDto noticeDto) throws AuthenticationException {
-		return ResponseEntity.ok().body(
-			service.insertNotice(noticeDto)
-		);
-	}
-	
-	@PatchMapping("/{noticeId}")
-	@ResponseBody
-	public ResponseEntity<UpdateResult> updateNoticeById(@PathVariable(name = "noticeId", required = true) long noticeId, NoticeDto noticeDto ) throws AuthenticationException {
-		return ResponseEntity.ok().body(
-			service.updateNoticeById(noticeId, noticeDto)
-		);
-	}
-	
-	@DeleteMapping("/{noticeId}")
-	@ResponseBody
-	public ResponseEntity<DeleteResult> deleteNoticeByNoticeId(@PathVariable(name = "noticeId", required = true) long noticeId ) throws AuthenticationException {
-		return ResponseEntity.ok().body(
-			service.deleteNoticeByNoticeId(noticeId)
-		);
-	}
-	
-	@PostMapping("/{noticeId}/reply")
-	@ResponseBody
-	public ResponseEntity<NoticeDto> insertReplyByNoticeId(@PathVariable(name = "noticeId", required = true) long noticeId, ReplyDto replyDto) throws AuthenticationException {
-		
-		return ResponseEntity.ok().body(
-			service.insertReplyByNoticeId(noticeId, replyDto)
-		);
-	}
-	
-	@PatchMapping("/{noticeId}/reply/{replyId}")
-	@ResponseBody
-	public ResponseEntity<NoticeDto> updateReplyByReplyId(@PathVariable(name = "noticeId", required = true) long noticeId, @PathVariable(name = "replyId", required = true) long replyId, ReplyDto replyDto) throws AuthenticationException {
-		
-		return ResponseEntity.ok().body(
-			service.updateReplyByReplyId(noticeId, replyId, replyDto)
-		);
-	}
-	
-	@DeleteMapping("/{noticeId}/reply/{replyId}")
-	@ResponseBody
-	public ResponseEntity<NoticeDto> deleteReplyByReplyId(@PathVariable(name = "noticeId", required = true) long noticeId, @PathVariable(name = "replyId", required = true) long replyId) throws AuthenticationException {
-		
-		return ResponseEntity.ok().body(
-			service.deleteReplyByReplyId(noticeId, replyId)
-		);
-	}
+      return ResponseEntity.ok().body(
+         service.insertNotice(noticeDto)
+      );
+   }
+   
+   @PatchMapping("/{noticeId}")
+   @ResponseBody
+   public ResponseEntity<UpdateResult> updateNoticeById(@PathVariable(name = "noticeId", required = true) long noticeId, @RequestBody NoticeDto noticeDto ) throws AuthenticationException {
+      
+      return ResponseEntity.ok().body(
+         service.updateNoticeById(noticeId, noticeDto)
+      );
+   }
+   
+   @DeleteMapping("/{noticeId}")
+   @ResponseBody
+   public ResponseEntity<DeleteResult> deleteNoticeByNoticeId(@PathVariable(name = "noticeId", required = true) long noticeId ) throws AuthenticationException {
+      return ResponseEntity.ok().body(
+         service.deleteNoticeByNoticeId(noticeId)
+      );
+   }
+   
+   @PostMapping("/{noticeId}/reply")
+   @ResponseBody
+   public ResponseEntity<NoticeDto> insertReplyByNoticeId(@PathVariable(name = "noticeId", required = true) long noticeId, @RequestBody ReplyDto replyDto) throws AuthenticationException {
+      
+      return ResponseEntity.ok().body(
+         service.insertReplyByNoticeId(noticeId, replyDto)
+      );
+   }
+   
+   @PatchMapping("/{noticeId}/reply/{replyId}")
+   @ResponseBody
+   public ResponseEntity<NoticeDto> updateReplyByReplyId(@PathVariable(name = "noticeId", required = true) long noticeId, @PathVariable(name = "replyId", required = true) long replyId, @RequestBody ReplyDto replyDto) throws AuthenticationException {
+   
+      return ResponseEntity.ok().body(
+         service.updateReplyByReplyId(noticeId, replyId, replyDto)
+      );
+   }
+   
+   @DeleteMapping("/{noticeId}/reply/{replyId}")
+   @ResponseBody
+   public ResponseEntity<NoticeDto> deleteReplyByReplyId(@PathVariable(name = "noticeId", required = true) long noticeId, @PathVariable(name = "replyId", required = true) long replyId) throws AuthenticationException {
+      
+      return ResponseEntity.ok().body(
+         service.deleteReplyByReplyId(noticeId, replyId)
+      );
+   }
 }
--- src/main/java/com/noti/api/user/controller/UserController.java
+++ src/main/java/com/noti/api/user/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,54 +24,54 @@ import lombok.AllArgsConstructor;
 @RequestMapping("user")
 @RestController
 public class UserController {
-	
-	UserService service;
-	
-	@GetMapping("")
-	@ResponseBody
-	public ResponseEntity<UserDto> getUserInfo( ) {
-		return ResponseEntity.ok().body(
-			service.getUserInfo()
-		);
-	}
-	
-	@PostMapping("/sign-in")
-	@ResponseBody
-	public ResponseEntity<UserDto> signIn( String userEmail, String userPwd ) {
-		return ResponseEntity.ok().body(
-			service.signIn(userEmail, userPwd)
-		);
-	}
-	
-	@PostMapping("/sign-up")
-	@ResponseBody
-	public ResponseEntity<UserDto> signUp(UserDto userDto) {
-		return ResponseEntity.ok().body(
-			service.signUp(userDto)
-		);
-	}
-	
-	@PostMapping("/sign-out")
-	@ResponseBody
-	public ResponseEntity<UserDto> signOut( ) {
-		return ResponseEntity.ok().body(
-			service.signOut()
-		);
-	}
-	
-	@PatchMapping("/{userId}")
-	@ResponseBody
-	public ResponseEntity<UpdateResult> userUpdate(@PathVariable(name = "userId", required = true) long userId,  UserDto updateDto) throws AuthenticationException {
-		return ResponseEntity.ok().body(
-			service.userUpdate(userId, updateDto)
-		);
-	}
-	
-	@DeleteMapping("/{userId}")
-	@ResponseBody
-	public ResponseEntity<DeleteResult> userDelete(@PathVariable(name = "userId", required = true) long userId) throws AuthenticationException {
-		return ResponseEntity.ok().body(
-			service.userDelete(userId)
-		);
-	}
+   
+   UserService service;
+   
+   @GetMapping("")
+   @ResponseBody
+   public ResponseEntity<UserDto> getUserInfo( ) {
+      return ResponseEntity.ok().body(
+         service.getUserInfo()
+      );
+   }
+   
+   @PostMapping("/sign-in")
+   @ResponseBody
+   public ResponseEntity<UserDto> signIn( String userEmail, String userPwd ) {
+      return ResponseEntity.ok().body(
+         service.signIn(userEmail, userPwd)
+      );
+   }
+   
+   @PostMapping("/sign-up")
+   @ResponseBody
+   public ResponseEntity<UserDto> signUp(@RequestBody UserDto userDto) {
+      return ResponseEntity.ok().body(
+         service.signUp(userDto)
+      );
+   }
+   
+   @PostMapping("/sign-out")
+   @ResponseBody
+   public ResponseEntity<UserDto> signOut( ) {
+      return ResponseEntity.ok().body(
+         service.signOut()
+      );
+   }
+   
+   @PatchMapping("/{userId}")
+   @ResponseBody
+   public ResponseEntity<UpdateResult> userUpdate(@PathVariable(name = "userId", required = true) long userId, @RequestBody UserDto updateDto) throws AuthenticationException {
+      return ResponseEntity.ok().body(
+         service.userUpdate(userId, updateDto)
+      );
+   }
+   
+   @DeleteMapping("/{userId}")
+   @ResponseBody
+   public ResponseEntity<DeleteResult> userDelete(@PathVariable(name = "userId", required = true) long userId) throws AuthenticationException {
+      return ResponseEntity.ok().body(
+         service.userDelete(userId)
+      );
+   }
 }
